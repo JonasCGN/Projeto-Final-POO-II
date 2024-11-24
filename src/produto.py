240e1fd482e4cb19cb9c2d390f73e8c7d8cebf46
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-11-24 15:17:50 -0300

    att produtos

--- src/produto.py
+++ src/produto.py
@@ -1,19 +1,28 @@
 import json
 
 class Produto:
-    def __init__(self, nome: str = None, preco: float = None) -> None:
+    def __init__(self, nome: str = None, preco: float = None, data: float = None, hora: float = None,quantidade: int = None) -> None:
         self.nome = nome
         self.preco = preco
+        self.data = data
+        self.hora = hora
+        self.quantidade = quantidade 
         
     def dump(self):
         return json.dumps({
             "nome": self.nome,
             "preco": self.preco,
+            "data": self.data,
+            "hora": self.hora,
+            "quantidade": self.quantidade,
         })
     
     def load(self, data: dict):
         self.nome = data["nome"]
         self.preco = data["preco"]
+        self.hora = data["hora"]
+        self.data = data["data"]
+        self.quantidade = data["quantidade"]
 
     def __str__(self) -> str:
         return f"{self.nome} - R${self.preco:.2f}"
@@ -23,15 +32,21 @@ class GerenciarProdutos:
     def __init__(self):
         self.produtos = {}
         
-    def add_produto(self, produto: Produto):
+    def add_produto(self, produto: Produto, data: float, hora: float, quantidade: int):
         self.produtos[produto.nome] = produto
+        self.produtos[produto.data] = data
+        self.produtos[produto.hora] = hora
+        self.produtos[produto.quantidade] = quantidade
         return True
         
     def listar_produtos(self):
         return [produto for produto in self.produtos.values()]
     
-    def comprar_produto(self, nome: str, qtd: int) -> float:
+    def comprar_produto(self, nome: str, qtd: int, data: float, hora: float) -> float:
         produto = self.produtos[nome]
+        quantidade = self.produtos[quantidade]
+        data = self.produtos[data]
+        hora = self.produtos[hora]
         return produto.preco * qtd
         
     def dump(self):

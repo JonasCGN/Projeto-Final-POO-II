821737267b6f62bbbd35e7e60a4fe4b3f1afbe29
KauaHenSilva <kaua.sbc@gmail.com>
2024-11-24 18:10:48 -0300

    feat: redis

--- script_redis/src/bd.py
+++ script_redis/src/bd.py
@@ -6,6 +6,17 @@ class DB_Redis:
     def __init__(self):
         self.redis_client = redis.Redis(
             host='redis', port=6379, decode_responses=True)
+        self.set_initial_values()
+
+    def set_initial_values(self):
+        self.qtd_removidos = self.get('qtd_removidos')
+        self.qtd_inseridos = self.get('qtd_inseridos')
+
+        if self.qtd_removidos is None:
+            self.insert('qtd_removidos', 0)
+
+        if self.qtd_inseridos is None:
+            self.insert('qtd_inseridos', 0)
 
     def test_connection(self):
         try:
@@ -13,3 +24,15 @@ class DB_Redis:
             print("Conexão com o Redis estabelecida com sucesso!")
         except redis.ConnectionError:
             print("Não foi possível conectar ao Redis.")
+
+    def insert(self, key, value):
+        self.redis_client.set(key, value)
+
+    def increment(self, key):
+        self.redis_client.incr(key)
+
+    def decrement(self, key):
+        self.redis_client.decr(key)
+
+    def get(self, key):
+        return self.redis_client.get(key)

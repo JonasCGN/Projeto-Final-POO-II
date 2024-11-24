5f28f25c72fd286a7f5701344ea4db0ead7023f9
JonasCGN <jonasbo66@gmail.com>
2024-11-24 18:05:00 -0300

    att

---
+++ test.py
@@ -0,0 +1,14 @@
+# import psycopg2
+# con = psycopg2.connect(host='localhost', database='regiao',
+# user='root', password='root')
+# cur = con.cursor()
+# sql = 'create table cidade (id serial primary key, nome varchar(100), uf varchar(2))'
+# cur.execute(sql)
+# sql = "insert into cidade values (default,'São Paulo,'SP')"
+# cur.execute(sql)
+# con.commit()
+# cur.execute('select * from cidade')
+# recset = cur.fetchall()
+# for rec in recset:
+#     print (rec)
+# con.close()

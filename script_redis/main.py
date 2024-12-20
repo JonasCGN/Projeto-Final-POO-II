4b92b25fbd24caa9db63caf00f17fa7ddf76df4a
KauaHenSilva <kaua.sbc@gmail.com>
2024-12-20 14:27:51 -0300

    feat: removendo arquivos desnecessarios

--- script_redis/main.py
+++
@@ -1,7 +0,0 @@
-from src.send_to_redis import SenderToRedis
-
-qtd_requests = 1_000_000
-
-if __name__ == '__main__':
-    sender = SenderToRedis()
-    sender.run(qtd_requests, 10)
--- worker/main.py
+++
@@ -1,8 +0,0 @@
-from libary.sincronizacao import Sincronizacao
-from time import sleep
-
-if __name__ == '__main__':
-    sincronizacao = Sincronizacao()
-    while True:
-        sincronizacao.run(10)
-        sleep(5)
